e132437bd1dc214268bc3d2c976500fb3745b801
tlucass <rm97913@fiap.com.br>
2023-08-22 10:12:05 -0300

    updateList

--- js/ex.js
+++ js/ex.js
@@ -3,19 +3,73 @@ const tasks = [];
 const addButton = document.querySelector("#addButton");
 const buttonImportance = document.querySelector("#buttonImportance");
 
+
+
+
+function updateList(taskList = tasks) {
+    const listElement = document.querySelector("#listaTarefa");
+    listElement.innerHTML = '';
+
+    //iterar por todas as tarefas em taskList
+    taskList.forEach(task => {
+        //Para cada tarefa, é criado um elemento li
+        const listItem = document.createElement("li");
+        //O conteúdo do item é preenchido com informações sobre a tarefa, incluindo descrição, autor, departamento e importância. 
+        listItem.textContent = `${task.description} - ${task.author} - ${task.department} - ${task.importance}`;
+        //Se a tarefa tiver um valor e duração, essas informações também são adicionadas ao texto do item.
+        if (task.value) listItem.textContent += ` - ${task.value}`;
+        if (task.duration) listItem.textContent += ` - ${task.duration}`;
+
+        let deleteButton = document.createElement("button");
+        deleteButton.textContent = " X ";
+        listItem.appendChild(deleteButton);
+
+        listElement.appendChild(listItem);
+
+        deleteButton.addEventListener("click", (evt) => {
+            evt.preventDefault();
+
+            //Encontra o índice da tarefa no array tasks
+            let index = tasks.indexOf(task);
+
+            if (index !== -1) {
+                tasks.splice(index, 1);
+            }
+
+            evt.target.parentNode.remove();
+        });
+    });
+    
+    document.querySelector("#description").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#department").value = "";
+    document.querySelector('input[name="importance"]:checked').checked = false;
+    document.querySelector("#value").value = "";
+    document.querySelector("#duration").value = "";
+}
+
+
+
+
+
 addButton.addEventListener("click", (event) => {
     event.preventDefault();
 
+    // Coleta os valores dos campos do formulário e  e armazen em variáveis JavaScript.
     const description = document.querySelector("#description").value;
     const author = document.querySelector("#author").value;
     const department = document.querySelector("#department").value;
     const value = document.querySelector("#value").value;
     const duration = document.querySelector("#duration").value;
-
+    
+    // Coleta todos os elementos com o nome "importance" no formulário. 
     const importanceEl = document.getElementsByName('importance');
     let importance;
+    
+    //Percorre todos os elementos de entrada de radio com o nome "importance"
     for (const element of importanceEl) {
         if (element.checked) {
+            //O valor do elemento selecionado é atribuído à variável importance
             importance = element.value;
             break;
         }
@@ -42,39 +96,3 @@ buttonImportance.addEventListener("click", () => {
     updateList(sortedTasks);
 });
 
-function updateList(taskList = tasks) {
-    const listElement = document.querySelector("#listaTarefa");
-    listElement.innerHTML = '';
-
-    taskList.forEach(task => {
-        const listItem = document.createElement("li");
-        listItem.textContent = `${task.description} - ${task.author} - ${task.department} - ${task.importance}`;
-        if (task.value) listItem.textContent += ` - ${task.value}`;
-        if (task.duration) listItem.textContent += ` - ${task.duration}`;
-
-        let deleteButton = document.createElement("button");
-        deleteButton.textContent = " X ";
-        listItem.appendChild(deleteButton);
-
-        listElement.appendChild(listItem);
-
-        deleteButton.addEventListener("click", (evt) => {
-            evt.preventDefault();
-
-            let index = tasks.indexOf(task);
-
-            if (index !== -1) {
-                tasks.splice(index, 1);
-            }
-
-            evt.target.parentNode.remove();
-        });
-    });
-    
-    document.querySelector("#description").value = "";
-    document.querySelector("#author").value = "";
-    document.querySelector("#department").value = "";
-    document.querySelector('input[name="importance"]:checked').checked = false;
-    document.querySelector("#value").value = "";
-    document.querySelector("#duration").value = "";
-}
